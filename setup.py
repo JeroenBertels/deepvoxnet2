693ea78f18c76079967a938638e8833baf8851bb
Jeroen Bertels <jeroen.bertels@gmail.com>
2020-11-24 13:40:33 +0100

    Updating setup.py script.

--- setup.py
+++ setup.py
@@ -11,16 +11,19 @@ setup(
     license='LGPL',
     packages=['deepvoxnet2'],
     zip_safe=False,
+    python_requires='>=3.6, <3.9',
     install_requires=[
-        'tensorflow>=2.3.1',
-        'tensorflow-addons>=0.11.2',
-        'scipy>=1.5.2',
-        'nibabel>=3.1.1',
-        'pydicom>=2.0.0',
-        'numba>=0.39.0',
+        'tensorflow>=2.3',
+        'tensorflow-addons>=0.11',
+        'numpy>=1.15,<1.19',
+        'scipy>=1.5',
+        'nibabel>=3.1',
+        'pydicom>=2.0',
+        'numba>=0.39',
         'matplotlib>=2.2.2',
-        'scikit-learn>=0.20.2',
-        'pandas>=0.24.1'
+        'scikit-image>=0.14'
     ],
-    dependency_links=['https://github.com/gschramm/pymirc']
+    dependency_links=[
+        'https://github.com/gschramm/pymirc'
+    ]
 )
