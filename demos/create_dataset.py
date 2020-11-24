1d39f28b10533455c7f937bb76b0f0b6441c9f62
Jeroen Bertels <jeroen.bertels@gmail.com>
2020-11-24 15:28:32 +0100

    Demos for creating samples and creating a dataset.

---
+++ demos/create_dataset.py
@@ -0,0 +1,33 @@
+import os
+from deepvoxnet2.components.mirc import Dataset, Case, Record, NiftyFileModality
+
+
+DEMO_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def create_dataset(data="train"):
+    if data == "train":
+        case_names = ["case_{}".format(i) for i in range(8)]
+
+    elif data == "val":
+        case_names = ["case_{}".format(i) for i in range(8, 10)]
+
+    else:
+        raise ValueError
+
+    dataset = Dataset("brats_2018_" + data, os.path.join(DEMO_DIR, "brats_2018"))
+    for case_name in case_names:
+        case = Case(case_name)
+        record = Record("record_0")
+        record.add(NiftyFileModality("flair", os.path.join(DEMO_DIR, "brats_2018", case_name, "FLAIR.nii.gz")))
+        record.add(NiftyFileModality("t1", os.path.join(DEMO_DIR, "brats_2018", case_name, "T1.nii.gz")))
+        record.add(NiftyFileModality("whole_tumor", os.path.join(DEMO_DIR, "brats_2018", case_name, "GT_W.nii.gz")))
+        case.add(record)
+        dataset.add(case)
+
+    return dataset
+
+
+if __name__ == '__main__':
+    brats_2018_train = create_dataset()
+    brats_2018_val = create_dataset('val')
---
+++ demos/create_samples.py
@@ -0,0 +1,124 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from deepvoxnet2.components.creator import Creator
+from deepvoxnet2.components.sample import Sample
+from deepvoxnet2.components.transformers import AffineDeformation, Put, RandomCrop, SampleInput, GridCrop, Crop, Flip, Group
+
+
+def create_samples():
+    # Let's define a toy example
+    x = np.zeros((100, 100))
+    x[25:50, 25:50] = 1
+    x[25:50, 50:75] = 2
+    y = np.zeros((100, 100))
+    y[25:50, 25:75] = 1
+    fig, axs = plt.subplots(1, 2)
+    axs[0].imshow(x, vmin=0, vmax=2)
+    axs[1].imshow(y, vmin=0, vmax=2)
+    plt.show()
+
+    # inputs
+    x_input = x_path = SampleInput(Sample(x))  # Sample converts x into a 5D array (batch, x, y, z, features) with an affine attribute (here just np.eye)
+    y_input = y_path = SampleInput(Sample(y))
+
+    # processing
+    x_path, y_path = AffineDeformation(x_path, translation_window_width=(10, 10, 0), rotation_window_width=(3.14 / 10, 0, 0))(x_path, y_path)
+    x_path_intermediate = x_path
+    x_path, y_path = RandomCrop(x_path, (27, 27, 1), n=16, nonzero=True)(x_path, y_path)
+    x_path, y_path = Flip((0.5, 0, 0))(x_path, y_path)
+    y_path = Crop(y_path, (21, 21, 1))(x_path)
+
+    # outputs
+    x_output = Put(y_input)(x_path)
+    y_output = y_input
+
+    # Let's visualize some x samples at the end of the processing pathway:
+    fig, axs = plt.subplots(3, 3)
+    for i in range(3):
+        x_sample = x_path.eval()[0]
+        x_sample_intermediate = x_path_intermediate[0]
+        axs[i, 0].imshow(x, vmin=0, vmax=2)
+        axs[i, 1].imshow(x_sample_intermediate[0, :, :, 0, 0], vmin=0, vmax=2)
+        axs[i, 2].imshow(x_sample[0, :, :, 0, 0], vmin=0, vmax=2)
+
+    plt.show()
+
+    # It was clear that the original x is affine transformed and then multiple patches are taken
+    # Suppose we also want to put the drawn sample back onto the original image space
+    fig, axs = plt.subplots(1, 4)
+    axs[0].imshow(x, vmin=0, vmax=2)
+    for i in range(3):
+        x_sample = x_output.eval()[0]
+        axs[i + 1].imshow(x_sample[0, :, :, 0, 0], vmin=0, vmax=2)
+
+    plt.show()
+
+    # Suppose we want to produce x and y samples, we must make sure that all transformers are updated (and not only the ones "below" x_path
+    # There are two options:
+    # Option 1: Just make sure they are grouped in one path and evaluate that path
+    samples_path = Group()([x_path, y_path])
+    fig, axs = plt.subplots(2, 4)
+    axs[0, 0].imshow(x, vmin=0, vmax=2)
+    axs[1, 0].imshow(y, vmin=0, vmax=2)
+    for i in range(3):
+        x_sample, y_sample = samples_path.eval()
+        axs[0, i + 1].imshow(x_sample[0, :, :, 0, 0], vmin=0, vmax=2)
+        axs[1, i + 1].imshow(y_sample[0, :, :, 0, 0], vmin=0, vmax=2)
+
+    plt.show()
+
+    # Option 2: Make a higher abstract Creator network and evaluate that network (this has the advantage that it only evaluates the nodes that are relevant and thus is more efficient)
+    creator = Creator([x_path, y_path])
+    fig, axs = plt.subplots(2, 4)
+    axs[0, 0].imshow(x)
+    axs[1, 0].imshow(y)
+    for i, output in enumerate(creator.eval()):
+        x_sample, y_sample = output
+        axs[0, i + 1].imshow(x_sample[0][0, :, :, 0, 0], vmin=0, vmax=2)
+        axs[1, i + 1].imshow(y_sample[0][0, :, :, 0, 0], vmin=0, vmax=2)
+        if i == 2:
+            break
+
+    plt.show()
+
+    # Finally, a Creator has notion of when your network has ran out of samples. As you can see, we ask 16 samples from the RandomCrop transformer (all the rest generate a default of 1 sample).
+    # This means the creator should create us 16 samples
+    creator = Creator([x_output, y_output])
+    fig, axs = plt.subplots(4, 4)
+    count = 0
+    for i, output in enumerate(creator.eval()):
+        x_sample, y_sample = output
+        axs[i // 4, i % 4].imshow(x_sample[0][0, :, :, 0, 0], vmin=0, vmax=2)
+        count += 1
+
+    plt.show()
+    assert count == 16
+
+    # Suppose we use a GridCrop instead of a RandomCrop with no n specified --> The GridCrop will create a number of samples as to complete the entire grid.
+    # inputs
+    x_input = x_path = SampleInput(Sample(x))
+    y_input = y_path = SampleInput(Sample(y))
+    # processing
+    x_path, y_path = AffineDeformation(x_path, translation_window_width=(10, 10, 0), rotation_window_width=(3.14 / 10, 0, 0))(x_path, y_path)
+    x_path, y_path = GridCrop(x_path, (27, 27, 1), strides=(21, 21, 1), nonzero=True)(x_path, y_path)
+    x_path, y_path = Flip((0.5, 0, 0))(x_path, y_path)
+    y_path = Crop(y_path, (21, 21, 1))(x_path)
+    # outputs
+    x_output = Put(y_input)(x_path)
+    y_output = y_input
+    creator = Creator([x_output, y_output])
+    fig, axs = plt.subplots(4, 4)
+    count = 0
+    for i, output in enumerate(creator.eval()):
+        x_sample, y_sample = output
+        axs[i // 4, i % 4].imshow(x_sample[0][0, :, :, 0, 0], vmin=0, vmax=2)
+        count += 1
+
+    plt.show()
+    print("They were {} samples from the grid cropper.".format(count))
+
+    # Have a look at how gradually the entire image got put back for the random and grid crop even though for both cases the samples came from an affine transformed input! :-)
+
+
+if __name__ == '__main__':
+    create_samples()
