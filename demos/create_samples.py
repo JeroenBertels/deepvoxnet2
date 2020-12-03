8d81857edc214cd60879c7215613c2b32f5b9ce3
Jeroen Bertels <jeroen.bertels@gmail.com>
2020-12-03 19:16:35 +0100

    Checked and updated + bugfixed create_samples.py demo.

--- demos/create_samples.py
+++ demos/create_samples.py
@@ -25,11 +25,11 @@ def create_samples():
     x_path, y_path = AffineDeformation(x_path, translation_window_width=(10, 10, 0), rotation_window_width=(3.14 / 10, 0, 0))(x_path, y_path)
     x_path_intermediate = x_path
     x_path, y_path = RandomCrop(x_path, (27, 27, 1), n=16, nonzero=True)(x_path, y_path)
-    x_path, y_path = Flip((0.5, 0, 0))(x_path, y_path)
-    y_path = Crop(y_path, (21, 21, 1))(x_path)
+    # x_path, y_path = Flip((0.5, 0, 0))(x_path, y_path)
+    y_path = Crop(y_path, (21, 21, 1))(y_path)
 
     # outputs
-    x_output = Put(y_input)(x_path)
+    x_output = Put(y_input, caching=False)(x_path)
     y_output = y_input
 
     # Let's visualize some x samples at the end of the processing pathway:
@@ -44,7 +44,7 @@ def create_samples():
     plt.show()
 
     # It was clear that the original x is affine transformed and then multiple patches are taken
-    # Suppose we also want to put the drawn sample back onto the original image space
+    # Suppose we also want to put the drawn sample back onto the original image space (to see what happens if we would instantiate the Put transformer with caching=True, see end of this demo ;-))
     fig, axs = plt.subplots(1, 4)
     axs[0].imshow(x, vmin=0, vmax=2)
     for i in range(3):
@@ -56,7 +56,7 @@ def create_samples():
     # Suppose we want to produce x and y samples, we must make sure that all transformers are updated (and not only the ones "below" x_path
     # There are two options:
     # Option 1: Just make sure they are grouped in one path and evaluate that path
-    samples_path = Group()([x_path, y_path])
+    samples_path = Group(2)([x_path, y_path])
     fig, axs = plt.subplots(2, 4)
     axs[0, 0].imshow(x, vmin=0, vmax=2)
     axs[1, 0].imshow(y, vmin=0, vmax=2)
@@ -70,8 +70,8 @@ def create_samples():
     # Option 2: Make a higher abstract Creator network and evaluate that network (this has the advantage that it only evaluates the nodes that are relevant and thus is more efficient)
     creator = Creator([x_path, y_path])
     fig, axs = plt.subplots(2, 4)
-    axs[0, 0].imshow(x)
-    axs[1, 0].imshow(y)
+    axs[0, 0].imshow(x, vmin=0, vmax=2)
+    axs[1, 0].imshow(y, vmin=0, vmax=2)
     for i, output in enumerate(creator.eval()):
         x_sample, y_sample = output
         axs[0, i + 1].imshow(x_sample[0][0, :, :, 0, 0], vmin=0, vmax=2)
@@ -99,7 +99,7 @@ def create_samples():
     x_input = x_path = SampleInput(Sample(x))
     y_input = y_path = SampleInput(Sample(y))
     # processing
-    x_path, y_path = AffineDeformation(x_path, translation_window_width=(10, 10, 0), rotation_window_width=(3.14 / 10, 0, 0))(x_path, y_path)
+    x_path, y_path = AffineDeformation(x_path, translation_window_width=(10, 10, 0), rotation_window_width=(0, 0, 0))(x_path, y_path)
     x_path, y_path = GridCrop(x_path, (27, 27, 1), strides=(21, 21, 1), nonzero=True)(x_path, y_path)
     x_path, y_path = Flip((0.5, 0, 0))(x_path, y_path)
     y_path = Crop(y_path, (21, 21, 1))(x_path)
