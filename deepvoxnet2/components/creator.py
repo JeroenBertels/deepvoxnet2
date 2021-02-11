b0c831862738e4f2ff647019c387e22cf545f064
Jeroen Bertels <jeroen.bertels@gmail.com>
2021-02-11 18:13:45 +0100

    Small bugfixes in the Creator and adding the possibility to write output samples to disk with the method "write_transformer_outputs"

--- deepvoxnet2/components/creator.py
+++ deepvoxnet2/components/creator.py
@@ -1,5 +1,7 @@
+import os
 import uuid
 import copy
+import nibabel as nib
 from deepvoxnet2.components.transformers import _Input, KerasModel
 
 
@@ -9,6 +11,7 @@ class Creator(object):
         self.outputs = Creator.deepcopy(outputs)
         self.active_transformers, self.active_connections = self.trace(self.outputs, set_active_indices=True, reset_transformers=True, set_names=True)
         self.active_input_transformers = [transformer for transformer in self.active_transformers if isinstance(transformer, _Input)]
+        self.sample_id = None
 
     def eval(self, identifier=None):
         for transformer in self.active_transformers:
@@ -19,11 +22,11 @@ class Creator(object):
 
         while True:
             try:
-                sample_id = uuid.uuid4()
+                self.sample_id = uuid.uuid4()
                 for output_connection in self.outputs:
-                    output_connection.eval(sample_id)
+                    output_connection.eval(self.sample_id)
 
-            except RuntimeError:
+            except StopIteration:
                 break
 
             yield copy.deepcopy([output.get() for output in self.outputs])
@@ -58,7 +61,9 @@ class Creator(object):
                     if set_names:
                         if connection.transformer.name is None:
                             connection.transformer.name = "{}_{}".format(connection.transformer.__class__.__name__, len([traced_transformer for traced_transformer in traced_transformers if traced_transformer.__class__.__name__ == connection.transformer.__class__.__name__]))
-                        
+
+                        assert connection.transformer.name not in [traced_transformer.name for traced_transformer in traced_transformers], "In a Creator you cannot use the same name for more than one Transformer."
+
                     traced_transformers.append(connection.transformer)
 
                 if set_active_indices and connection.idx not in connection.transformer.active_indices:
@@ -95,18 +100,34 @@ class Creator(object):
         return output_connections
 
     def summary(self):
-        print('_' * 210)
-        print('{:<50} {:<10} {:<75} {:<75}'.format('Transformer (type) (n)', '#I/O', 'Output Shape', 'Connected to'))
-        print('=' * 210)
-        for transformer in self.active_transformers:
+        print('_' * 260)
+        print('{:<50} {:<10} {:<75} {:<75} {:<50}'.format('Transformer (type) (n)', 'idx', 'Output shape', 'Connected to', 'Extra connected to'))
+        print('=' * 260)
+        for transformer in reversed(self.active_transformers):
             for i, idx in enumerate(transformer.active_indices):
-                if len(transformer.connections[idx]) == 0:
-                    print('{:<50} {:<10} {:<75} {:<75}'.format("" if i > 0 else f"{transformer.name} ({transformer.__class__.__name__}) ({transformer.n})", idx, str(transformer.output_shapes[idx]), ""))
+                print('{:<50} {:<10} {:<75} {:<75} {:<50}'.format(
+                    '"' if i > 0 else f"{transformer.name} ({transformer.__class__.__name__}) ({transformer.n})",
+                    idx,
+                    str(transformer.output_shapes[idx]),
+                    str([f"{connection.transformer.name}[{connection.idx}]" for connection in transformer.connections[idx]]),
+                    '"' if i > 0 else str([f"{connection.transformer.name}[{connection.idx}]" for connection in transformer.extra_connections])))
 
-                else:
-                    for idx_, connection in enumerate(transformer.connections[idx]):
-                        print('{:<50} {:<10} {:<75} {:<75}'.format("" if i > 0 else f"{transformer.name} ({transformer.__class__.__name__}) ({transformer.n})", idx, str(transformer.output_shapes[idx]), f"{connection.transformer.name}[{connection.idx}]"))
-
-            print('_' * 210)
+            print('_' * 260)
 
         print('')
+
+    def write_transformer_outputs(self, output_dir, file_format=".nii.gz"):
+        if file_format not in [".nii.gz"]:
+            raise ValueError
+
+        sample_dir = os.path.join(output_dir, str(self.sample_id))
+        assert not os.path.isdir(sample_dir), "The transformer outputs have already been written to the specified directory."
+        os.makedirs(sample_dir)
+        for transformer in self.active_transformers:
+            for idx in transformer.active_indices:
+                for idx_, sample in enumerate(transformer[idx]):
+                    if len(sample) > 1:
+                        print(f"WARNING: encountered a batch size = {len(sample)} > 1 for {transformer.name} at idx {idx} and idx_ {idx_} --> only batch element 0 is saved.")
+
+                    img = nib.Nifti1Image(sample[0], affine=sample.affine[0])
+                    nib.save(img, os.path.join(sample_dir, f"{transformer.name}_idx-{idx}_output-{idx_}" + file_format))
