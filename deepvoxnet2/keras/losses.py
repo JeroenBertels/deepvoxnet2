1ec7d352bae8a3546b022180080f11a20c522045
Jeroen Bertels <jeroen.bertels@gmail.com>
2021-02-15 17:30:10 +0100

    Bugfixes to metrics and losses and keeping only already checked.

--- deepvoxnet2/keras/losses.py
+++ deepvoxnet2/keras/losses.py
@@ -1,22 +1,13 @@
-from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
-from deepvoxnet2.keras.metrics import binary_dice_score, categorical_dice_score
-
-
-def binary_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
-    return BinaryCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
+from deepvoxnet2.keras.metrics import binary_crossentropy, categorical_crossentropy, binary_dice_score
 
 
 def binary_dice_loss(y_true, y_pred, sample_weight=None, **kwargs):
-    kwargs["threshold"] = kwargs.get("threshold", None)
-    assert kwargs["threshold"] is None, "A dice loss function needs to use soft outputs and thus no thresholding operation can be done."
-    return 1 - binary_dice_score(y_true, y_pred, sample_weight=sample_weight, **kwargs)
+    return 1 - binary_dice_score(y_true, y_pred, sample_weight=sample_weight, threshold=None, **kwargs)
 
 
-def categorical_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
-    return CategoricalCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
-
-
-def categorical_dice_loss(y_true, y_pred, sample_weight=None, **kwargs):
-    kwargs["threshold"] = kwargs.get("threshold", None)
-    assert kwargs["threshold"] is None, "A dice loss function needs to use soft outputs and thus no thresholding operation can be done."
-    return 1 - categorical_dice_score(y_true, y_pred, sample_weight=sample_weight, **kwargs)
+# def categorical_dice_loss(y_true, y_pred, sample_weight=None, **kwargs):
+#     kwargs["threshold"] = kwargs.get("threshold", None)
+#     assert kwargs["threshold"] is None, "A dice loss function needs to use soft outputs and thus no thresholding operation can be done."
+#     kwargs["reduce_along_features"] = kwargs.get("reduce_along_features", False)
+#     assert kwargs["reduce_along_features"] is False, "The categorical version of the dice loss/score should treat every feature/class as one sample to compute the dice loss/score on."
+#     return 1 - categorical_dice_score(y_true, y_pred, sample_weight=sample_weight, **kwargs)
--- deepvoxnet2/keras/metrics.py
+++ deepvoxnet2/keras/metrics.py
@@ -1,39 +1,43 @@
 import tensorflow as tf
-from tensorflow.keras.metrics import BinaryAccuracy, AUC
+from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
+from tensorflow.keras.metrics import BinaryAccuracy, AUC, CategoricalAccuracy
 from pymirc.metrics.tf_metrics import generalized_dice_coeff
+
+
+def auc(y_true, y_pred, sample_weight=None, **kwargs):
+    return AUC(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
 def binary_accuracy(y_true, y_pred, sample_weight=None, **kwargs):
     return BinaryAccuracy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
-def binary_dice_score(y_true, y_pred, sample_weight=None, **kwargs):
-    kwargs["threshold"] = kwargs.get("threshold", 0.5)
-    return generalized_dice_coeff(y_true, y_pred, **kwargs)
+def binary_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
+    return BinaryCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
-def binary_auc(y_true, y_pred, sample_weight=None, **kwargs):
-    kwargs["multi_label"] = kwargs.get("multi_label", False)
-    assert kwargs['multi_label'] is False, "A binary AUC is just treating every point in the array as a sample. However it makes no difference here, please be consistent and put multi_label to False."
-    return AUC(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
+def binary_dice_score(y_true, y_pred, sample_weight=None, threshold=0.5, **kwargs):
+    return generalized_dice_coeff(y_true, y_pred, reduce_along_features=True, threshold=threshold, **kwargs)
 
 
-def binary_volume_difference(y_true, y_pred, sample_weight=None, threshold=None):
-    if threshold is not None:
-        y_true = tf.cast(tf.math.greater(y_true, threshold), y_true.dtype)
-        y_pred = tf.cast(tf.math.greater(y_pred, threshold), y_pred.dtype)
-
-    return tf.math.reduce_sum(y_pred, axis=(1, 2, 3, 4)) - tf.math.reduce_sum(y_true, axis=(1, 2, 3, 4))
-
-
-def categorical_dice_score(y_true, y_pred, sample_weight=None, **kwargs):
-    kwargs["threshold"] = kwargs.get("threshold", 0.5)
-    kwargs["reduce_along_features"] = kwargs.get("reduce_along_features", False)
-    assert kwargs['reduce_along_features'] is False, "If a reduction is performed across the feature/class dimension, then you are actually calculating a binary dice score on N+1 dimensions."
-    return generalized_dice_coeff(y_true, y_pred, **kwargs)
+# def binary_volume_difference(y_true, y_pred, sample_weight=None, threshold=None):
+#     if threshold is not None:
+#         y_true = tf.cast(tf.math.greater(y_true, threshold), y_true.dtype)
+#         y_pred = tf.cast(tf.math.greater(y_pred, threshold), y_pred.dtype)
+#
+#     return tf.math.reduce_sum(y_pred, axis=(1, 2, 3, 4)) - tf.math.reduce_sum(y_true, axis=(1, 2, 3, 4))
 
 
-def categorical_auc(y_true, y_pred, sample_weight=None, **kwargs):
-    kwargs["multi_label"] = kwargs.get("multi_label", True)
-    assert kwargs['multi_label'] is True, "As opposed to the binary AUC, the categorical AUC should result in the average AUC of each feature/class and thus multi_label should be set to True."
-    return AUC(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
+def categorical_accuracy(y_true, y_pred, sample_weight=None, **kwargs):
+    return CategoricalAccuracy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
+
+
+def categorical_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
+    return CategoricalCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
+
+
+# def categorical_dice_score(y_true, y_pred, sample_weight=None, **kwargs):
+#     kwargs["threshold"] = kwargs.get("threshold", 0.5)
+#     kwargs["reduce_along_features"] = kwargs.get("reduce_along_features", False)
+#     assert kwargs["reduce_along_features"] is False, "The categorical version of the dice loss/score should treat every feature/class as one sample to compute the dice loss/score on."
+#     return generalized_dice_coeff(y_true, y_pred, **kwargs)
