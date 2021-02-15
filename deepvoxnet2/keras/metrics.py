a8295e30fb6432dfd83450eedb6df370dfcb139c
Jeroen Bertels <jeroen.bertels@gmail.com>
2021-02-15 15:13:57 +0100

    Added metrics: - binary_accuracy

--- deepvoxnet2/keras/metrics.py
+++ deepvoxnet2/keras/metrics.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
+from tensorflow.keras.metrics import BinaryAccuracy
 from pymirc.metrics.tf_metrics import generalized_dice_coeff
 
 
-def binary_dice_score(y_true, y_pred, sample_weight=None, **kwargs):
-    if "threshold" not in kwargs:
-        kwargs["threshold"] = 0.5
+def binary_accuracy(y_true, y_pred, sample_weight=None, **kwargs):
+    return BinaryAccuracy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
+
+def binary_dice_score(y_true, y_pred, sample_weight=None, **kwargs):
+    kwargs["threshold"] = kwargs.get("threshold", 0.5)
     return generalized_dice_coeff(y_true, y_pred, **kwargs)
 
 
