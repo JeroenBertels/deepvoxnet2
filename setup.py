e2e3a470ef95faefa65651b3514a4249b13b99b6
Jeroen Bertels <jeroen.bertels@gmail.com>
2021-02-16 12:01:09 +0100

    Change to setup.py to include also sub directories as packages Updated version to 2.1.4

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setuptools.setup(
     name='deepvoxnet2',
-    version='2.1.3',
+    version='2.1.4',
     description='Deep learning processing framework for Keras.',
     url='https://github.com/JeroenBertels/deepvoxnet2',
     author='Jeroen Bertels, David Robben',
     author_email='jeroen.bertels@gmail.com',
     license='LGPL',
-    packages=['deepvoxnet2'],
+    packages=setuptools.find_packages(),
     zip_safe=False,
     python_requires='>=3.6, <3.9',
     install_requires=[
