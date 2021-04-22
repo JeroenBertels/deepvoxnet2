e43b6bda1ccb06aa1e645d74488055974666eec2
Jeroen Bertels <jeroen.bertels@kuleuven.be>
2021-04-22 16:23:27 +0200

    Possibility to construct custom functions using get_... instead of having to use functools partial

--- deepvoxnet2/keras/losses.py
+++ deepvoxnet2/keras/losses.py
@@ -1,18 +1,25 @@
+from functools import partial
 from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
-from deepvoxnet2.keras.metrics import binary_dice_score, categorical_dice_score
+from deepvoxnet2.keras.metrics import categorical_dice_score, binary_dice_score
 
 
 def binary_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
     return BinaryCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
-def binary_dice_loss(y_true, y_pred, sample_weight=None, **kwargs):
-    return 1 - binary_dice_score(y_true, y_pred, sample_weight=sample_weight, threshold=None, **kwargs)
+def binary_dice_loss(y_true, y_pred, sample_weight=None, threshold=None, **kwargs):
+    return 1 - binary_dice_score(y_true, y_pred, sample_weight=sample_weight, threshold=threshold, **kwargs)
+
+
+def get_binary_dice_loss(threshold=None, **kwargs):
+    loss = partial(binary_dice_loss, threshold=threshold, **kwargs)
+    loss.__name__ = "binary_dice_loss"
+    return loss
 
 
 def categorical_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
     return CategoricalCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
-def categorical_dice_loss(y_true, y_pred, sample_weight=None, **kwargs):
-    return 1 - categorical_dice_score(y_true, y_true * y_pred, sample_weight=sample_weight, threshold=None, **kwargs)
+def categorical_dice_loss(y_true, y_pred, sample_weight=None, threshold=None, **kwargs):
+    return 1 - categorical_dice_score(y_true, y_true * y_pred, sample_weight=sample_weight, threshold=threshold, **kwargs)
--- deepvoxnet2/keras/metrics.py
+++ deepvoxnet2/keras/metrics.py
@@ -1,10 +1,7 @@
+from functools import partial
 import tensorflow as tf
 from tensorflow.keras.metrics import AUC, CategoricalAccuracy, BinaryCrossentropy, CategoricalCrossentropy
 from pymirc.metrics.tf_metrics import generalized_dice_coeff
-
-
-# def auc(y_true, y_pred, sample_weight=None, **kwargs):
-#     return AUC(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
 def binary_accuracy(y_true, y_pred, sample_weight=None, threshold=0.5, **kwargs):
@@ -16,15 +13,17 @@ def binary_accuracy(y_true, y_pred, sample_weight=None, threshold=0.5, **kwargs)
     return tf.reduce_sum(sample_weight * tf.cast(tf.math.equal(y_true, y_pred), y_true.dtype), axis=(1, 2, 3, 4)) / tf.reduce_sum(sample_weight, axis=(1, 2, 3, 4))
 
 
-# def binary_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
-#     return BinaryCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
-
-
 def binary_dice_score(y_true, y_pred, sample_weight=None, threshold=0.5, **kwargs):
     if sample_weight is not None:
         raise NotImplementedError
 
     return generalized_dice_coeff(y_true, y_pred, threshold=threshold, **kwargs)
+
+
+def get_binary_dice_score(threshold=0.5, **kwargs):
+    metric = partial(binary_dice_score, threshold=threshold, **kwargs)
+    metric.__name__ = "binary_dice_score"
+    return metric
 
 
 def binary_true_positives(y_true, y_pred, sample_weight=None, threshold=0.5, **kwargs):
@@ -97,14 +96,6 @@ def binary_volume_difference(y_true, y_pred, sample_weight=None, threshold=0.5, 
 
 def binary_abs_volume_difference(y_true, y_pred, sample_weight=None, threshold=0.5, voxel_volume=1, **kwargs):
     return tf.abs(binary_pred_volume(y_true, y_pred, sample_weight=sample_weight, threshold=threshold, voxel_volume=voxel_volume) - binary_true_volume(y_true, y_pred, sample_weight=sample_weight, threshold=threshold, voxel_volume=voxel_volume))
-
-
-# def categorical_accuracy(y_true, y_pred, sample_weight=None, **kwargs):
-#     return CategoricalAccuracy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
-
-
-# def categorical_crossentropy(y_true, y_pred, sample_weight=None, **kwargs):
-#     return CategoricalCrossentropy(**kwargs)(y_true, y_pred, sample_weight=sample_weight)
 
 
 def categorical_dice_score(y_true, y_pred, sample_weight=None, exclude_background=True, threshold='argmax',  **kwargs):
