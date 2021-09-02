7f9cea8f68cdc824b0aa1bfd2504bb8899b32b44
JeroenBertels <jeroen.bertels@kuleuven.be>
2021-09-02 18:54:08 +0200

    Updated Sampler and sub classes: - now supports weighted sampling and more natural way "shuffle" and "randomize" organization

--- deepvoxnet2/components/sampler.py
+++ deepvoxnet2/components/sampler.py
@@ -34,9 +34,12 @@ class SampleIdentifier(dict):
 
 
 class Sampler(object):
-    def __init__(self, identifiers=None, shuffle=False):
+    def __init__(self, identifiers=None, shuffle=False, weights=None):
         self.identifiers = [] if identifiers is None else identifiers
+        self.base_identifiers = self.identifiers
         self.shuffle = shuffle
+        self.weights = weights
+        self.randomize()
 
     def __len__(self):
         return len(self.identifiers)
@@ -48,27 +51,40 @@ class Sampler(object):
         return iter(self.identifiers)
 
     def randomize(self):
+        self._randomize()
         if self.shuffle:
-            self._randomize()
+            if self.weights is None:
+                random.shuffle(self.base_identifiers)
+
+            else:
+                _ = list(zip(self.base_identifiers, self.weights))
+                random.shuffle(_)
+                self.base_identifiers, self.weights = zip(*_)
+
+        if len(self.base_identifiers) > 0 and self.weights is not None:
+            assert len(self.base_identifiers) == len(self.weights), "When sample weights are specified, you must specify a weight for each identifier."
+            self.identifiers = random.choices(self.base_identifiers, weights=self.weights, k=len(self.base_identifiers))
+
+        else:
+            self.identifiers = self.base_identifiers
 
     def _randomize(self):
-        raise NotImplementedError
+        pass
 
 
 class MircSampler(Sampler):
     def __init__(self, mirc, mode="per_record", **kwargs):
-        super(MircSampler, self).__init__(**kwargs)
         self.mirc = mirc
         self.mode = mode
-        self._randomize()
+        super(MircSampler, self).__init__(**kwargs)
 
     def _randomize(self):
-        identifiers = []
+        self.base_identifiers = []
         if self.mode == "per_record":
             for dataset_id in self.mirc:
                 for case_id in self.mirc[dataset_id]:
                     for record_id in self.mirc[dataset_id][case_id]:
-                        identifiers.append(MircIdentifier(self.mirc, dataset_id, case_id, record_id))
+                        self.base_identifiers.append(MircIdentifier(self.mirc, dataset_id, case_id, record_id))
 
         elif self.mode == "per_case":
             for dataset_id in self.mirc:
@@ -76,19 +92,14 @@ class MircSampler(Sampler):
                     record_id_i = random.randint(0, len(self.mirc[dataset_id][case_id]) - 1)
                     for i, record_id in enumerate(self.mirc[dataset_id][case_id]):
                         if i == record_id_i:
-                            identifiers.append(MircIdentifier(self.mirc, dataset_id, case_id, record_id))
+                            self.base_identifiers.append(MircIdentifier(self.mirc, dataset_id, case_id, record_id))
                             break
 
         else:
             raise NotImplementedError
-
-        self.identifiers = identifiers
 
 
 class SampleSampler(Sampler):
     def __init__(self, samples, **kwargs):
         identifiers = [SampleIdentifier(sample) for sample in samples]
         super(SampleSampler, self).__init__(identifiers, **kwargs)
-
-    def _randomize(self):
-        random.shuffle(self.identifiers)
