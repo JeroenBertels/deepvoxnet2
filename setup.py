83e827ac9dba45471c15f505a66bb88c370b0743
JeroenBertels <jeroen.bertels@kuleuven.be>
2021-11-11 18:10:28 +0100

    Updated to version deepvoxnet-2.9.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='deepvoxnet2',
-    version='2.9.10',
+    version='2.9.11',
     description='Deep learning processing framework for Keras.',
     url='https://github.com/JeroenBertels/deepvoxnet2',
     author='Jeroen Bertels, David Robben',
