9251ee6963c2910b93ce05efdc5c4d99ca155df5
JeroenBertels <jeroen.bertels@kuleuven.be>
2022-07-29 16:33:01 +0200

    Relaxed requirement of python version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     license='LGPL',
     packages=setuptools.find_packages(),
     zip_safe=False,
-    python_requires='>=3.9, <3.10',
+    python_requires='>=3.8, <3.10',
     install_requires=[
         'tensorflow>=2.4,<2.8',
         'tensorflow-addons>=0.11',
