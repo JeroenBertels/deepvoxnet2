Small bugfix to allow default value to be also array/tuple/list broadcastable with S size (and as such to work with newer versions of Crop transformers

deepvoxnet2/utilities/transformations.py
@@ -188,7 +188,7 @@ def crop(I, S_size, coordinates=None, subsample_factors=None, default_value=0, p
         idx_S[i] = slice(start_S, end_S + 1)
 
     S[tuple(idx_S)] = I[tuple(idx_I)]
-    if np.isnan(default_value):
+    if np.any(np.isnan(default_value)):
         S = S[tuple(distance_transform_edt(np.isnan(S), return_distances=False, return_indices=True))]
 
     return S